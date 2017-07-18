abe8757d2e4df6c5a39e84093f78fdc94e3dc7c8
Mike McDonald <mikemcd97@gmail.com>
2017-07-18 21:54:43 +0000

    going local

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,23 +1,44 @@
 var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var myReddit = require('../lib/reddit.js');
+app.set('view engine', 'pug');
+
 
 module.exports = function(myReddit) {
     var authController = express.Router();
     
     authController.get('/login', function(request, response) {
-        response.send("TO BE IMPLEMENTED");
+        response.render("login-form");
     });
     
-    authController.post('/login', function(request, response) {
-        response.send("TO BE IMPLEMENTED");
+    authController.post('/login', urlencodedParser, function(request, response) {
+        var input = request.body
+        myReddit.checkUserLogin(input.username, input.password)
+        .then(response =>{
+            myReddit.createUserSession(response.Id)
+           
+        })
+        .then(
+            response.redirect('/')
+            );
     });
     
     authController.get('/signup', function(request, response) {
-        response.send("TO BE IMPLEMENTED");
+        response.render('signup-form');
     });
     
-    authController.post('/signup', function(request, response) {
-        response.send("TO BE IMPLEMENTED");
+    authController.post('/signup', urlencodedParser, function(request, response) {
+       var input = request.body;
+       myReddit.createUser({
+           username: input.username,
+           password: input.password
+           })
+        .then(
+            response.redirect('/auth/login')
+            );
     });
     
     return authController;
-}
+};
